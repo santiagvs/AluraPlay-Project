faeff88377ab581c396dcf3b5ca561af85588597
Santiago <victor.lacsilva0102@gmail.com>
2023-01-26 16:01:37 -0300

    feat/refactor: adding PSR7 http-messages and refactoring Controller class to accept ServerRequestInterface and ResponseInterface

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Alura\Mvc\Controller;
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 interface Controller
 {
-    public function processRequest(): void;
+    public function processRequest(ServerRequestInterface $request): ResponseInterface;
 }
