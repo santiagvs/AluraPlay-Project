b90e1fd846fff1630ea319cc67dcf6e32c951492
Santiago <victor.lacsilva0102@gmail.com>
2023-01-27 20:31:51 -0300

    feat: adding Plates template engine and setting it into container

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,6 +1,7 @@
 <?php
 
 use DI\ContainerBuilder;
+use League\Plates\Engine;
 
 $builder = new ContainerBuilder;
 $builder->addDefinitions([
@@ -9,7 +10,11 @@ $builder->addDefinitions([
     $pdo = new PDO("mysql:host={$config['dbhost']};dbname={$config['dbname']}", "{$config['dbuser']}", "{$config['dbpass']}");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $pdo;
-    }
+  },
+  Engine::class => function () {
+    $templatePath = __DIR__ . '/../views';
+    return new Engine($templatePath);
+  }
 ]);
 
 $container = $builder->build();
